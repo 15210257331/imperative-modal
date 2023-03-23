de3e6688475d79df1c076abf2c4e60e868991735
chenxiaofei <15210257331@163.com>
2023-03-23 10:01:42 +0800

    fix:config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Markdown from 'vite-plugin-md';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),vueJsx()],
+  plugins: [
+    vue(
+      {
+        include: [/\.vue$/, /\.md$/],
+      }
+    ),
+    vueJsx(),
+    Markdown()
+  ],
   build: {
     target: "esnext",
     outDir: 'lib',
     lib: {
       entry: "packages/dialog/index.ts",
       name: "vue3-dialog",
-      formats: ["es", "cjs","umd"],
+      formats: ["es", "cjs", "umd"],
       fileName: (format) => `vue3-dialog.${format}.js`,
     },
     rollupOptions: {
