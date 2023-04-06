2fcd19d89af147e2bc3a99db245100baaa13643b
chenxiaofei <15210257331@163.com>
2023-04-06 14:25:28 +0800

    fix:调整弹窗的样式

--- src/components/DemoDialog.vue
+++ src/components/DemoDialog.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <div style="height:200px">
+        <div style="height:200px; text-align:left;">
             <p>{{ id }}</p>
             s拉科技砥砺奋进猎杀对决反垃圾手打拉法基拉手机大发路阿拉斯加地方啦吉利丁粉进啦手打阿里卡世纪东方垃圾十六大飞机拉萨Component多路阀老司机component
             收到菲拉斯接待来访拉上快点解封辣椒水
             虽然从鼠标位置出现的动画看起来很酷，但是我们有时也想要一些朴实无华的从屏幕中间弹出来的动画。这时你可以将 transform-origin 设为 'center'。
         </div>
         <div style="display: flex; justify-content: flex-end;">
-            <el-button type="default" @click="cancel"> 取消</el-button>
-            <el-button type="primary" @click="confirm"> 提交</el-button>
+            <button  @click="cancel"> 取消</button>
+            <button  @click="confirm"> 提交</button>
         </div>
     </div>
 </template>
