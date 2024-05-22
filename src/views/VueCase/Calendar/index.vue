01a334862bc2bb75ee2dadf82258cf1ebe8ea07f
chenxiaofei <15210257331@163.com>
2024-05-22 19:40:49 +0800

    feat: 日历组件&& 分片上传

---
+++ src/views/VueCase/Calendar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ date }}</p>
+  <el-button @click="handleSet">设置值</el-button>
+  <Calendar v-model="date" ref="calendar" />
+</template>
+
+<script setup lang="ts">
+import Calendar from '../../../../packages/Calendar/index.vue'
+import { computed, onMounted, ref } from 'vue'
+
+const date = ref(new Date('2024-5-24'))
+
+const calendar = ref()
+
+function handleSet() {
+  calendar.value.setDate(new Date(2024, 6, 18))
+}
+
+onMounted(() => {
+  console.log(calendar.value.getDate())
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/VueCase/index.vue
+++ src/views/VueCase/index.vue
@@ -6,9 +6,16 @@
 import { ref, markRaw, watch, computed, onMounted } from 'vue'
 import CaseList from '@/components/CaseList.vue'
 import SliceUpload from './SliceUpload/index.vue'
+import SliceUpload1 from './SliceUpload1/index.vue'
 import GradientBorder from './GradientBorder/index.vue'
+import Calendar from './Calendar/index.vue'
 
 const caseList = ref([
+  {
+    title: '日历',
+    component: markRaw(Calendar),
+    path: ['VueCase/Calendar/index']
+  },
   {
     title: '渐变边框',
     component: markRaw(GradientBorder),
@@ -18,6 +25,11 @@ const caseList = ref([
     title: '大文件切片上传',
     component: markRaw(SliceUpload),
     path: ['VueCase/SliceUpload/index']
+  },
+  {
+    title: '大文件切片上传、秒传、断点续传',
+    component: markRaw(SliceUpload1),
+    path: ['VueCase/SliceUpload1/index']
   }
 ])
 </script>
